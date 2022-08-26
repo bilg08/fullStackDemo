2038f9a5cf1a73ef7f502488b5310a6e43c255a2
bilguunZorigooLeap1 <bilguunzorigooleap1@iMac2021-02s-iMac.local>
2022-08-26 10:30:37 +0800

    gif

---
+++ snake/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import {Gif} from "./gif/gif"
+function App() {
+  return (
+    <div className="App">
+   <Gif/>
+    </div>
+  );
+}
+
+export default App;
