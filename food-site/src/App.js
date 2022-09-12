f881cc3acbc1d86d15bf635fc3627e051727b561
bilguunZorigooLeap1 <bilguunzorigooleap1@iMac2021-02s-iMac.local>
2022-09-12 12:05:06 +0800

    started food site

---
+++ food-site/src/App.js
@@ -0,0 +1,9 @@
+import PersistentDrawerLeft from "./pages/adminHomePage/adminHomePage";
+
+export const App=()=> {
+  return (
+    <div className="App">
+       <PersistentDrawerLeft/>
+    </div>
+  );
+}
